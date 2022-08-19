2732836617ae86182542079bdea008c765ca4f33
gisellesouzaa <giselle_souza97@yahoo.com.br>
2022-08-19 12:37:03 -0300

    adicionado calculadora

--- desafio5.js
+++ desafio5.js
@@ -5,4 +5,11 @@ object.addEventListener('click',clicar)
 function clicar() {
     alert('Resolução desafio 5')
 
+    // Primeiramente, a pessoa deverá escolher uma opção de operação impressa pelo programa na tela.As opções disponíveis deverão ser: soma, subtração, multiplicação, divisão, e sair. Nessa última, o programa deverá parar de ser executado, mostrando uma mensagem "Até a próxima".
+
+    // Depois, ela deverá inserir os dois valores que deseja utilizar, e o programa imprimirá o resultado da operação em questão.
+
+
+
+
 }
