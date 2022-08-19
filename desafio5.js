12203f89e1e45ced40a1661e045b90cbc4c7da7f
gisellesouzaa <giselle_souza97@yahoo.com.br>
2022-08-19 14:48:04 -0300

    desafio 5 js

--- desafio5.js
+++ desafio5.js
@@ -1,15 +1,66 @@
 let object = document.querySelector('input#desafio5')
 
-object.addEventListener('click',clicar)
+object.addEventListener('click', clicar)
 
 function clicar() {
-    alert('Resolução desafio 5')
+    // Funções
+    function soma(n1, n2) {
+        return Number(n1) + Number(n2)
 
-    // Primeiramente, a pessoa deverá escolher uma opção de operação impressa pelo programa na tela.As opções disponíveis deverão ser: soma, subtração, multiplicação, divisão, e sair. Nessa última, o programa deverá parar de ser executado, mostrando uma mensagem "Até a próxima".
+    }
 
-    // Depois, ela deverá inserir os dois valores que deseja utilizar, e o programa imprimirá o resultado da operação em questão.
+    function subtratrai(n1, n2) {
+        return Number(n1) - Number(n2)
+    }
 
+    function multiplica(n1, n2) {
+        return Number(n1) * Number(n2)
+    }
 
+    function divide(n1, n2) {
+        return Number(n1) / Number(n2)
+    }
 
+    let n1 = "";
+    let n2= "";
+    let operacao = "";
 
+    do {
+        operacao = prompt("Escolha a operação matemática: \n 1 soma \n 2 subtração \n 3 multiplicação \n 4 divisão \n 5 sair")
+
+        while (operacao != "1" && operacao != "2" && operacao != "3" && operacao != "4" && operacao != "5") {
+            alert(`Operação não reconhecida!`);
+            operacao = prompt(`Escolha a operação matemática que você quer realizar: \n 1 soma \n 2 subtração \n 3 multiplicação \n 4 divisão \n 5 sair"`);
+            }
+        if (operacao == "5"){
+            break            
+        }
+
+        let n1 = prompt("Digite o primeiro valor para a operação:")
+        let n2 = prompt("Digite o segundo valor para a operação:")
+
+        const tipoCalculo = operacao;
+        switch (tipoCalculo) {
+            case '1':
+                let resultado1 = soma(n1, n2)
+                alert(`A soma de ${n1} + ${n2} = ${resultado1}`);
+                break;
+            case '2':
+                let resultado2 = subtratrai(n1, n2)
+                alert(`A subtração de ${n1} - ${n2} = ${resultado2}`);
+                break;
+            case '3':
+                let resultado3 = multiplica(n1, n2)
+                alert(`A multiplicação de ${n1} x ${n2} = ${resultado3}`);
+                break;
+            case '4':
+                let resultado4 = divide(n1, n2)
+                alert(`A divisão de ${n1} / ${n2} = ${resultado4}`);
+                break;
+            // default:
+            //     break;
+        }
+    } while(operacao === "1" || operacao === "2" || operacao === "3" || operacao === "4");
+    alert("Até a próxima!");
 }
+
